23dc09fe2e0bec8347904e1e96daa8c31bd60029
mskKote <52584520+mskKote@users.noreply.github.com>
2019-08-06 18:30:00 +0300

    Add files via upload
    С работающей кнопкой

--- JS/UIcontroller.js
+++ JS/UIcontroller.js
@@ -1,2 +1,16 @@
 "use strict"
 
+let light_state = "ON";
+
+function theme(){
+    if (light_state === "ON") {
+        light_state = "OFF";
+        let light = document.getElementById("theme");
+        light.removeAttribute("href");
+        }
+    else {
+        light_state = "ON";
+        let light = document.getElementById("theme");
+        light.setAttribute("href", "CSS/color_theme_dark.css");
+    }
+}
